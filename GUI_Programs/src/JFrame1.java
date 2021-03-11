71139470705e6ad874584c74f225cfc66ba9bb48
LazyMan13 <rwesleyhenderson@gmail.com>
2021-03-11 10:02:51 -0600

    First Commit

---
+++ GUI_Programs/src/JFrame1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class JFrame1
+{
+	public static void main(String[] args)
+	{
+		JFrame aFrame = new JFrame("First Name");
+		aFrame.setSize(250, 100);
+		aFrame.setVisible(true);
+		JFrame.setDefaultLookAndFeelDecorated(true);
+	}
+}
