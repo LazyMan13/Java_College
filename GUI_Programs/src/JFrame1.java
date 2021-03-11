0a08321cbbf5c7f14b3f5e04f59106f9ec1ee0e2
LazyMan13 <rwesleyhenderson@gmail.com>
2021-03-11 10:20:56 -0600

    Final Dimension ints Added greeting with changable fonts

--- GUI_Programs/src/JFrame1.java
+++ GUI_Programs/src/JFrame1.java
@@ -1,12 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JFrame1
 {
 	public static void main(String[] args)
 	{
-		JFrame aFrame = new JFrame("First Name");
-		aFrame.setSize(250, 100);
-		aFrame.setVisible(true);
+		final int FRAME_WIDTH = 250;
+		final int FRAME_HEIGHT = 100;
+		Font headlineFont = new Font("Ariel", Font.BOLD, 36);
 		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame aFrame = new JFrame("First Name");
+		aFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		aFrame.setVisible(true);
+		aFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel greeting = new JLabel("Good day");
+		greeting.setFont(headlineFont);
+		aFrame.add(greeting);
 	}
 }
